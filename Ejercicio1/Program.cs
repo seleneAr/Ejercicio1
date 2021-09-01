ddfaeef4bf98b674b1e0c38dfa818e01bac56bef
Selene <selene.ajtd@gmail.com>
2021-09-01 21:02:29 +0200

    ejercicio1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ejercicio1
 {
@@ -6,7 +7,48 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car = new Car();
+            Motorbike motorbike = new Motorbike();
+            Truck truck = new Truck();
+
+            List<IVehiclable> vehicles = new List<IVehiclable>();
+            vehicles.Add(car);
+            vehicles.Add(motorbike);
+            vehicles.Add(truck);
+
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine(vehicle.Fabricar());
+            }
+        }
+
+        interface IVehiclable
+        {
+            string Fabricar();
+        }
+
+        class Car : IVehiclable
+        {
+            public string Fabricar()
+            {
+                return "Coche fabricado";
+            }
+        }
+
+        class Motorbike : IVehiclable
+        {
+            public string Fabricar()
+            {
+                return "Moto fabricada";
+            }
+        }
+
+        class Truck : IVehiclable
+        {
+            public string Fabricar()
+            {
+                return "Camión fabricado";
+            }
         }
     }
 }
